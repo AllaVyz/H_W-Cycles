cbcb7b327bfafcd911040145196d9e842651acc8
AllaVyz <efialka@mail.ru>
2024-02-24 19:32:33 +0300

    third commit

--- src/test/java/ru/netology/service/VacationServiceTest.java
+++ src/test/java/ru/netology/service/VacationServiceTest.java
@@ -27,7 +27,7 @@ import ru.netology.service.VacationService;
         int income = 100000;
         int expenses = 60000;
         int threshold = 150000;
-        int expected = 3;
+        int expected = 2;
         int actual = service.calculateVacations(income, expenses, threshold);
 
         Assertions.assertEquals(expected, actual);
